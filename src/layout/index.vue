d43f7c59acb9eae1850e9f2eb44e1c1b9713f0b1
lobo819 <1490297242@qq.com>
2022-10-19 01:28:20 +0800

    项目架构

---
+++ src/layout/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Header from './header/index.vue'
+import Content from './content/index.vue'
+</script>
+<template>
+  <div>
+    <Header></Header>
+    <Content></Content>
+  </div>
+</template>
